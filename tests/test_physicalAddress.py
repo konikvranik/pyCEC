02bef5752ba5a6ff7e0d1460fd22e505814c547a
vranikp <petr.vranik@lmc.eu>
2016-12-13 18:01:13 +0100

    assertTrue

--- tests/test_physicalAddress.py
+++ tests/test_physicalAddress.py
@@ -6,16 +6,16 @@ from pycec import PhysicalAddress
 class TestPhysicalAddress(TestCase):
     def test_aslist(self):
         pa = PhysicalAddress("8f:ab")
-        self.failUnless(pa.aslist == [0x8, 0xf, 0xa, 0xb])
+        self.assertTrue(pa.aslist == [0x8, 0xf, 0xa, 0xb])
 
     def test_asint(self):
         pa = PhysicalAddress("8f:ab")
-        self.failUnless(pa.asint == 0x8fab)
+        self.assertTrue(pa.asint == 0x8fab)
 
     def test_ascmd(self):
         pa = PhysicalAddress("8f:ab")
-        self.failUnless(pa.ascmd == "8f:ab")
+        self.assertTrue(pa.ascmd == "8f:ab")
 
     def test_str(self):
         pa = PhysicalAddress("8f:ab")
-        self.failUnless(("%s" % pa) == "8.f.a.b")
+        self.assertTrue(("%s" % pa) == "8.f.a.b")
