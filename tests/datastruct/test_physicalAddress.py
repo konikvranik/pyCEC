1e48cb3e3071318d7a9a1cf3ca3c2d4c37595fd2
vranikp <petr.vranik@lmc.eu>
2016-12-13 20:28:41 +0100

    assert equal

--- tests/datastruct/test_physicalAddress.py
+++ tests/datastruct/test_physicalAddress.py
@@ -6,16 +6,16 @@ from pycec.datastruct import PhysicalAddress
 class TestPhysicalAddress(TestCase):
     def test_aslist(self):
         pa = PhysicalAddress("8f:ab")
-        self.assertTrue(pa.aslist == [0x8, 0xf, 0xa, 0xb])
+        self.assertEqual(pa.aslist, [0x8, 0xf, 0xa, 0xb])
 
     def test_asint(self):
         pa = PhysicalAddress("8f:ab")
-        self.assertTrue(pa.asint == 0x8fab)
+        self.assertEqual(pa.asint, 0x8fab)
 
     def test_ascmd(self):
         pa = PhysicalAddress("8f:ab")
-        self.assertTrue(pa.ascmd == "8f:ab")
+        self.assertEqual(pa.ascmd, "8f:ab")
 
     def test_str(self):
         pa = PhysicalAddress("8f:ab")
-        self.assertTrue(("%s" % pa) == "8.f.a.b")
+        self.assertEqual(("%s" % pa), "8.f.a.b")
--- tests/test_hdmiDevice.py
+++ tests/test_hdmiDevice.py
@@ -7,30 +7,30 @@ from pycec import HdmiDevice
 
 
 class TestHdmiDevice(TestCase):
-    pass
 
-    #    def test_logical_address(self):
-    #        self.fail()
+    def test_logical_address(self):
+        device = HdmiDevice(2)
+        self.assertEqual(device.logical_address, 2)
 
     def test_update(self):
         device = HdmiDevice(2)
         cmd = CecCommand('02:%02x:4f:6e:6b:79:6f:20:48:54:58:2d:32:32:48:44:58' % CMD_OSD_NAME[1])
         device.update(cmd)
-        self.assertTrue(device.name == 'Onkyo HTX-22HDX')
+        self.assertEqual(device.name, 'Onkyo HTX-22HDX')
 
         cmd = CecCommand('02:%02x:01' % CMD_POWER_STATUS[1])
         device.update(cmd)
-        self.assertTrue(device.power_status == 1)
+        self.assertEqual(device.power_status, 1)
         cmd = CecCommand('02:%02x:02' % CMD_POWER_STATUS[1])
         device.update(cmd)
-        self.assertTrue(device.power_status == 2)
+        self.assertEqual(device.power_status, 2)
 
         cmd = CecCommand('02:%02x:18:C0:86' % CMD_VENDOR[1])
         device.update(cmd)
-        self.assertTrue(device.vendor_id == 0x18C086)
-        self.assertTrue(device.vendor == 'Broadcom')
+        self.assertEqual(device.vendor_id, 0x18C086)
+        self.assertEqual(device.vendor, 'Broadcom')
 
         cmd = CecCommand('02:%02x:C0:86' % CMD_PHYSICAL_ADDRESS[1])
         device.update(cmd)
-        self.assertTrue(device.physical_address.ascmd == 'c0:86')
-        self.assertTrue(device.physical_address.aslist == [0xC, 0x0, 0x8, 0x6])
+        self.assertEqual(device.physical_address.ascmd, 'c0:86')
+        self.assertEqual(device.physical_address.aslist, [0xC, 0x0, 0x8, 0x6])
