d51c52f5b4d2198644ca610e292d0a916a42884a
Robbie Trencheny <me@robbiet.us>
2017-01-23 19:16:04 -0800

    Sort VENDORS list, Add Apple and Roku

--- pycec/const.py
+++ pycec/const.py
@@ -1,30 +1,32 @@
-VENDORS = {0x000039: 'Toshiba',
-           0x0000F0: 'Samsung',
-           0x0005CD: 'Denon',
-           0x000678: 'Marantz',
-           0x000982: 'Loewe',
-           0x0009B0: 'Onkyo',
-           0x000CB8: 'Medion',
-           0x000CE7: 'Toshiba',
-           0x001582: 'PulseEight',
-           0x001950: 'HarmanKardon',
-           0x001A11: 'Google',
-           0x0020C7: 'Akai',
-           0x002467: 'AOC',
-           0x008045: 'Panasonic',
-           0x00903E: 'Philips',
-           0x009053: 'Daewoo',
-           0x00A0DE: 'Yamaha',
-           0x00D0D5: 'Grundig',
-           0x00E036: 'Pioneer',
-           0x00E091: 'LG',
-           0x08001F: 'Sharp',
-           0x080046: 'Sony',
-           0x18C086: 'Broadcom',
-           0x534850: 'Sharp',
-           0x6B746D: 'Vizio',
-           0x8065E9: 'Benq',
-           0x9C645E: 'HarmanKardon',
+VENDORS = {0x0020C7: 'Akai'
+           0x0010FA: 'Apple',
+           0x002467: 'AOC'
+           0x8065E9: 'Benq'
+           0x18C086: 'Broadcom'
+           0x009053: 'Daewoo'
+           0x0005CD: 'Denon'
+           0x001A11: 'Google'
+           0x00D0D5: 'Grundig'
+           0x001950: 'Harman Kardon'
+           0x9C645E: 'Harman Kardon'
+           0x00E091: 'LG'
+           0x000982: 'Loewe'
+           0x000678: 'Marantz'
+           0x000CB8: 'Medion'
+           0x0009B0: 'Onkyo'
+           0x008045: 'Panasonic'
+           0x00903E: 'Philips'
+           0x00E036: 'Pioneer'
+           0x001582: 'Pulse Eight'
+           0x8AC72E: 'Roku',
+           0x0000F0: 'Samsung'
+           0x08001F: 'Sharp'
+           0x534850: 'Sharp'
+           0x080046: 'Sony'
+           0x000039: 'Toshiba'
+           0x000CE7: 'Toshiba'
+           0x6B746D: 'Vizio'
+           0x00A0DE: 'Yamaha'
            0: 'Unknown'}
 
 CEC_LOGICAL_TO_TYPE = [0,  # TV0
