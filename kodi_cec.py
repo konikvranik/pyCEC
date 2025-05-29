108a500ad33b4ea70909b75ff7f959965e558f46
Petr Vraník <Petr.Vranik@mgm-tp.com>
2025-05-29 21:48:22 +0200

    Refactor Kodi CEC adapter initialization.
    Simplified and cleaned up the `KodiAdapter` class by removing unused imports and redundant code related to CEC initialization and adapter handling. Streamlined logging

--- kodi_cec.py
+++ kodi_cec.py
@@ -2,16 +2,17 @@
 """
 Hlavní vstupní bod CEC TCP serveru
 """
-import asyncio
 import os
 import sys
+
 import xbmc
 import xbmcaddon
 import xbmcvfs
 
-from pycec.server import CECServerProtocol, CECServer
-from pycec.cec import CecAdapter
-from pycec.network import HDMINetwork, AbstractCecAdapter
+from pycec.commands import KeyPressCommand, CecCommand
+from pycec.const import ADDR_RECORDINGDEVICE1
+from pycec.network import AbstractCecAdapter
+from pycec.server import CECServer
 
 # Získání instance addonu a cest
 ADDON = xbmcaddon.Addon()
@@ -31,21 +32,11 @@ sys.path.insert(0, RESOURCES_LIB_DIR)
 def log(msg, level=xbmc.LOGINFO):
     xbmc.log("[{}] {}".format(ADDON_ID, msg), level)
 
+
 class KodiAdapter(AbstractCecAdapter):
-    def __init__(self, name: str = None, monitor_only: bool = None,
-                 activate_source: bool = None,
+    def __init__(self, name: str = None, monitor_only: bool = None, activate_source: bool = None,
                  device_type=ADDR_RECORDINGDEVICE1):
         super().__init__()
-        self._adapter = None
-        self._io_executor = ThreadPoolExecutor(1)
-        import cec
-        self._cecconfig = cec.libcec_configuration()
-        if monitor_only is not None:
-            self._cecconfig.bMonitorOnly = 1 if monitor_only else 0
-        self._cecconfig.strDeviceName = name[:13]
-        if activate_source is not None:
-            self._cecconfig.bActivateSource = 1 if activate_source else 0
-        self._cecconfig.deviceTypes.Add(device_type)
 
     def set_command_callback(self, callback):
         self._cecconfig.SetKeyPressCallback(
@@ -78,37 +69,15 @@ class KodiAdapter(AbstractCecAdapter):
             self._adapter.CommandFromString(command.raw))
 
     def init(self, callback: callable = None):
-        return self._loop.run_in_executor(self._io_executor, self._init,
-                                          callback)
+        return self._loop.run_in_executor(self._io_executor, self._init, callback)
 
     def _init(self, callback: callable = None):
-        import cec
-        if not self._cecconfig.clientVersion:
-            self._cecconfig.clientVersion = cec.LIBCEC_VERSION_CURRENT
-        _LOGGER.debug("Initializing CEC...")
-        adapter = cec.ICECAdapter.Create(self._cecconfig)
-        _LOGGER.debug("Created adapter")
+        log("Initializing CEC...")
+        log("Created adapter")
         a = None
-        adapters = adapter.DetectAdapters()
-        for a in adapters:
-            _LOGGER.info("found a CEC adapter:")
-            _LOGGER.info("port:     " + a.strComName)
-            _LOGGER.info("vendor:   " + (
-                VENDORS[a.iVendorId] if a.iVendorId in VENDORS else hex(
-                    a.iVendorId)))
-            _LOGGER.info("product:  " + hex(a.iProductId))
-            a = a.strComName
-        if a is None:
-            _LOGGER.warning("No adapters found")
-        else:
-            if adapter.Open(a):
-                _LOGGER.info("connection opened")
-                self._adapter = adapter
-                self._initialized = True
-            else:
-                _LOGGER.error("failed to open a connection to the CEC adapter")
         if callback:
             callback()
+
 
 class CecServerService(xbmc.Monitor):
     """
