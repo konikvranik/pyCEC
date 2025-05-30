2be22bd39a797fd5be586b8720305c53eecf7576
Petr Vraník <Petr.Vranik@mgm-tp.com>
2025-05-30 09:59:02 +0200

    Refactor callbacks to use async methods and rename functions
    Updated callback handling to utilize asyncio for better concurrency.
    Renamed `set_command_callback` to `set_on_command_callback` for clarity
    and consistency, alongside replacing `update_callback` with `async_update`.

--- tests/test_hdmi_device.py
+++ tests/test_hdmi_device.py
@@ -19,23 +19,23 @@ def test_update():
         "02:%02x:4f:6e:6b:79:6f:20:48:54:58:2d:32:32:48:44:58"
         % CMD_OSD_NAME[1]
     )
-    device.update_callback(cmd)
+    device.async_update(cmd)
     assert device.osd_name == "Onkyo HTX-22HDX"
 
     cmd = CecCommand("02:%02x:01" % CMD_POWER_STATUS[1])
-    device.update_callback(cmd)
+    device.async_update(cmd)
     assert device.power_status == 1
     cmd = CecCommand("02:%02x:02" % CMD_POWER_STATUS[1])
-    device.update_callback(cmd)
+    device.async_update(cmd)
     assert device.power_status == 2
 
     cmd = CecCommand("02:%02x:18:C0:86" % CMD_VENDOR[1])
-    device.update_callback(cmd)
+    device.async_update(cmd)
     assert device.vendor_id == 0x18C086
     assert device.vendor == "Broadcom"
 
     cmd = CecCommand("02:%02x:C0:86:01" % CMD_PHYSICAL_ADDRESS[1])
-    device.update_callback(cmd)
+    device.async_update(cmd)
     assert device.physical_address.ascmd == "c0:86"
     assert device.physical_address.asattr == [0xC0, 0x86]
 
@@ -84,27 +84,27 @@ def test_type_name():
 
 def test_update_callback():
     device = HDMIDevice(3)
-    device.update_callback(
+    device.async_update(
         CecCommand(CMD_PHYSICAL_ADDRESS[1], att=[0x11, 0x00, 0x02])
     )
     assert "1.1.0.0" == str(device.physical_address)
     assert 2 == device.type
-    device.update_callback(CecCommand(CMD_POWER_STATUS[1], att=[0x01]))
+    device.async_update(CecCommand(CMD_POWER_STATUS[1], att=[0x01]))
     assert 1 == device.power_status
     assert device.is_off is True
     assert device.is_on is False
-    device.update_callback(CecCommand(CMD_POWER_STATUS[1], att=[0x00]))
+    device.async_update(CecCommand(CMD_POWER_STATUS[1], att=[0x00]))
     assert 0 == device.power_status
     assert device.is_on is True
     assert device.is_off is False
-    device.update_callback(CecCommand(CMD_POWER_STATUS[1], att=[0x02]))
+    device.async_update(CecCommand(CMD_POWER_STATUS[1], att=[0x02]))
     assert 2 == device.power_status
     assert device.is_on is False
     assert device.is_off is False
-    device.update_callback(
+    device.async_update(
         CecCommand(CMD_OSD_NAME[1], att=list(map(lambda x: ord(x), "Test4")))
     )
     assert "Test4" == device.osd_name
-    device.update_callback(CecCommand(CMD_VENDOR[1], att=[0x00, 0x80, 0x45]))
+    device.async_update(CecCommand(CMD_VENDOR[1], att=[0x00, 0x80, 0x45]))
     assert 0x008045 == device.vendor_id
     assert "Panasonic" == device.vendor
